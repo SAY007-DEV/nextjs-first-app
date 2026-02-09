cf6f221015d6962b8afd78bceec08f9bfc0def30
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-02-09 10:39:41 +0530

    about page added

---
+++ first/app/about/Page.jsx
@@ -0,0 +1,5 @@
+export const AboutPage = (params) => {
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>About Page</h1>
+    </div>
+}
